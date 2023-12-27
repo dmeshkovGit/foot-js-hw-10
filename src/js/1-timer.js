bdf15d22bff2f0aafd69f5cdc037af689ccf4f4c
dmeshkovGit <140300183+dmeshkovGit@users.noreply.github.com>
2023-12-27 16:27:41 +0200

    start

---
+++ src/js/1-timer.js
@@ -0,0 +1,10 @@
+// import flatpickr from "flatpickr";
+// import "flatpickr/dist/flatpickr.min.css";
+
+// flatpickr("#datetime-picker", {enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//   },});
